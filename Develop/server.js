bc03bcf11691de6f7aca388ad29df589825eff35
Nico Pasqualini <pasqualinin9@gmail.com>
2022-02-16 15:41:52 +1100

    initial schema and server setup

---
+++ Develop/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+require('dotenv').config()
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
+
+
+
+
+
+// Query database
+db.query('SELECT * FROM employee', function (err, results) {
+  console.log(results);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
